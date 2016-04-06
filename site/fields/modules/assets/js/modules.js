e04bdb721cd02a754520c548c5daaa138b5ab4c7
frekw <fredrik@soundtrackyourbrand.com>
2016-04-06 14:52:51 +0200

    Updated kirby-modules

--- site/fields/modules/assets/js/modules.js
+++ site/fields/modules/assets/js/modules.js
@@ -11,35 +11,65 @@
     return targetForHref($(el).attr('href'));
   };
 
-  var Tabs = function(el) {
+  var Tabs = function(el, options) {
+    var defaults = {
+      filter: '*'
+    };
+
+    this.options = $.extend({}, defaults, options)
+
     this.$tabs = $(el);
 
     this.toggle = this.toggle.bind(this);
 
-    this.$tabs.on('click', 'a', function(e){
+    this.$tabs.on('click', this.options.filter + ' a', function(e){
       e.preventDefault();
-      this.toggle(e.target);
+      e.stopPropagation();
+
+      this.toggle(e.currentTarget);
+
+      if(this.options.onChange) this.options.onChange(e);
     }.bind(this));
 
-    this.toggle(this.$tabs.find('a').first());
+    if(this.items().find('a').length < 1){
+      return;
+    }
+
+    if(this.items().find('a[data-active="data-active"]').length > 0){
+      this.items().find('a').each(function(){
+        targetForLink(this).hide();
+      });
+
+      this.activate(this.items().find('a[data-active="data-active"]'));
+    } else {
+      this.toggle(this.items().find('a').first());
+    }
   }
+
+  Tabs.prototype.items = function(){
+    return this.$tabs.find(this.options.filter);
+  }
+
+  Tabs.prototype.activate = function($target){
+    targetForLink($target).show();
+    this.$active = $target.attr('data-active', 'data-active');
+  };
 
   Tabs.prototype.toggle = function(target) {
     var $target = $(target);
 
     if(this.$active) this.$active.attr('data-active', null);
 
-    this.$tabs.find('a').each(function(){
+    this.items().find('a').each(function(){
       targetForLink(this).hide();
     })
 
-    targetForLink($target).show();
-    this.$active = $target.attr('data-active', 'data-active');
+    this.activate($target);
   };
 
-  $.fn.tabs = function(){
+  $.fn.tabs = function(options){
     return this.each(function(){
-      new Tabs(this);
+      new Tabs(this, options);
     });
   };
 })(jQuery);
@@ -57,21 +87,47 @@
     this.toggle = this.toggle.bind(this);
 
     this.$el.on('click', options.toggle, function(e){
+      if($(e.target).is(this.options.ignore)) {
+        return;
+      }
+
       e.preventDefault();
       e.stopPropagation();
 
-      this.toggle($(e.target));
+      this.toggle($(e.currentTarget));
+
+      if(this.options.onChange) {
+        this.options.onChange(e);
+      }
+    }.bind(this));
+
+    this.$el.find(options.toggle).each(function(i, e){
+      var $el = $(e);
+
+      if($el.is('.accordion--closed')){
+        this.close($el);
+      } else {
+        this.open($el);
+      }
     }.bind(this));
   };
 
   Accordion.prototype.toggle = function($target){
     if($target.is('.accordion--closed')){
-      $target.removeClass('accordion--closed').addClass('accordion--open');
-      $target.next(this.options.content).show();
+      this.open($target);
     } else {
-      $target.removeClass('accordion--open').addClass('accordion--closed');
-      $target.next(this.options.content).hide();
+      this.close($target);
     }
+  };
+
+  Accordion.prototype.open = function($target){
+    $target.removeClass('accordion--closed').addClass('accordion--open');
+    $target.next(this.options.content).show();
+  };
+
+  Accordion.prototype.close = function($target){
+    $target.removeClass('accordion--open').addClass('accordion--closed');
+    $target.next(this.options.content).hide();
   };
 
   $.fn.accordion = function(options){
@@ -86,17 +142,57 @@
 
 (function($) {
 
+  var last = function(arr){
+    if(arr.length < 1) return null;
+
+    return arr[arr.length - 1];
+  }
+
+  var first = function(arr) {
+    if(!arr.length) return null;
+
+    return arr[0]
+  }
+
   var Modules = function(el) {
     var element  = $(el);
     var api      = element.data('api');
     var sortable = element.data('sortable');
 
-    element.find('.tabs').tabs();
+    element.find('.tabs').tabs({
+      filter: '.tab',
+      onChange: function(e){
+        var $el = $(e.currentTarget);
+        var parts = $el.attr('href').split('-');
+        var id = last(parts);
+        var tab = first(parts.splice(-2, 1));
+
+        $('input[name$="[' + id + '][_editor_state][active_tab]"]').val(tab);
+        $el.closest('form').trigger('keep');
+      }
+    });
+
     element.accordion({
-      toggle: '> .modules-entries > .modules-entry > .accordion-toggle'
+      ignore: '.modules-entry-delete, .modules-entry-delete i',
+      toggle: '> .modules-entries > .modules-entry > .accordion-toggle',
+      onChange: function(e){
+        var $el = $(e.currentTarget);
+        var parts = $el.closest('.modules-entry').attr('id').split('-');
+        var id = last(parts);
+        var collapsed = $el.is('.accordion--closed');
+
+        $('input[name$="[' + id + '][_editor_state][collapsed]"]').val(collapsed);
+        $el.closest('form').trigger('keep');
+      }
     });
 
     if(sortable === false) return false;
+
+    // This is really ugly. Oh well.
+    var cb = function(){ $(this).closest('form').trigger('keep'); };
+    element.find('> .modules-entries > .modules-actions > .modules-add-button').on('click', cb);
+    element.find('> .modules-entries > .modules-empty > p > .modules-empty-add-button').on('click', cb);
+    element.find('> .modules-entries > .modules-entry .modules-entry-delete').on('click', cb);
 
 
     element.sortable({
--- site/fields/modules/templates/items.php
+++ site/fields/modules/templates/items.php
@@ -1,38 +1,45 @@
 <?php
 $form = require implode(DS, array(__DIR__, 'form.php'));
 
-foreach($field->entries() as $i => $entry): ?>
+foreach($field->entries() as $i => $entry):
+  $state = isset($entry->_editor_state) ? $entry->_editor_state : array('active_tab' => '', 'collapsed' => '');
+?>
 
 <div class="modules-entry" id="modules-entry-<?php echo $entry->id() ?>">
-  <h4 class="modules-type accordion-toggle accordion--open"><?php echo ucfirst($entry->type()); ?> Module</h4>
-    <div class="accordion-content">
-
-    <?php if($form('hasOptions', $entry, $field)): ?>
+  <div class="modules-header accordion-toggle<?php e($state['collapsed'] === 'true', ' accordion--closed') ?>">
+    <h4 class="modules-type"><?php echo ucfirst($entry->type()); ?> Module</h4>
     <ul class="tabs">
-      <li><a href="#modules-entry-fields-<?php echo $entry->id() ?>">Content</a></li>
-      <li><a href="#modules-entry-options-<?php echo $entry->id() ?>">Settings</a></li>
+      <?php if($form('has-options', $entry, $field)): ?>
+      <li class="tab"><a href="#modules-entry-fields-<?php echo $entry->id() ?>" <?php e($state['active_tab'] !== 'options', 'data-active="data-active"') ?>"><?php i('pencil') ?></a></li>
+      <li class="tab"><a href="#modules-entry-options-<?php echo $entry->id() ?>" <?php e($state['active_tab'] === 'options', 'data-active="data-active"') ?>"><?php i('cog') ?></a></li>
+      <?php endif; ?>
+      <li><a href="#" class="toggle-on"><?php i('toggle-on') ?></a></li>
+      <li><a href="#" class="toggle-off"><?php i('toggle-off') ?></a></li>
+      <?php if(!$field->readonly()): ?>
+        <li>
+          <a data-modal
+             class="modules-entry-delete"
+             href="<?php __($field->url('delete') . '/' . $entry->id()) ?>">
+            <?php i('trash') ?>
+          </a>
+        </li>
+      <?php endif; ?>
     </ul>
-    <?php endif; ?>
+  </div>
 
-    <?php if(!$field->readonly()): ?>
-      <a data-modal
-         class="btn btn-icon btn-rounded btn-negative modules-entry-delete"
-         href="<?php __($field->url('delete') . '/' . $entry->id()) ?>">
-      <?php i('trash-o', 'left') . _l('fields.structure.delete') ?>
-    </a>
-    <?php endif; ?>
-
+  <div class="accordion-content">
     <div class="modules-entry-content">
       <?php if(!$field->readonly()): ?>
         <div class="modules-entry-fields" id="modules-entry-fields-<?php echo $entry->id() ?>">
           <?php $form('fields', $entry, $field); ?>
         </div>
 
-        <?php if($form('hasOptions', $entry, $field)): ?>
+        <?php if($form('has-options', $entry, $field)): ?>
           <div class="modules-entry-options" id="modules-entry-options-<?php echo $entry->id() ?>">
             <?php $form('options', $entry, $field); ?>
           </div>
         <?php endif; ?>
+      <?php $form('editor-state', $entry, $field); ?>
       <?php require implode(DS, array(__DIR__, 'form.php')); ?>
      <?php endif ?>
   </div>
